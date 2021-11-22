a9aeb18a0bcc2fe72e1c40f7a4a0b52f459d8e6a
santiagomenesses <72476704+santiagomenesses@users.noreply.github.com>
2021-11-22 18:59:53 -0500

    AgregarHC

--- demo-protal/src/views/AgregarHC.vue
+++ demo-protal/src/views/AgregarHC.vue
@@ -1,8 +1,36 @@
 <template>
   <div class="wrapper-layout">
     <Header title="Agregar Historia Clinica" class="header"></Header>
-    <nav>Barra</nav>
-    <section>contenido</section>
+
+    <nav class="sidebar">Barra</nav>
+
+    <section class="content">
+      <div class="print-data">
+        <label for="idMascota" class="tag-input-login">Id Mascota</label>
+        <input type="text" class="input-login" id="idMascota" />
+      </div>
+      <div class="print-data">
+        <label for="idMedico" class="tag-input-login">Id Medico</label>
+        <input type="text" class="input-login" id="idMedico" />
+      </div>
+      <div class="print-data">
+        <label for="fecha" class="tag-input-login">Fecha</label>
+        <input type="text" class="input-login" id="fecha" />
+      </div>
+      <div class="print-data">
+        <label for="edad" class="tag-input-login">Edad</label>
+        <input type="text" class="input-login" id="edad" />
+      </div>
+      <div class="write-data">
+        <label for="descripcion" class="tag-input-login">Descripcion</label>
+        <input type="text" class="input-login" id="descripcion" />
+      </div>
+      <div class="write-data">
+        <label for="Receta" class="tag-input-login">Receta</label>
+        <input type="text" class="input-login" id="Receta" />
+      </div>
+      <input type="submit" value="Guardar" class="submit-info" />
+    </section>
   </div>
 </template>
 
@@ -28,5 +56,63 @@ export default defineComponent({
   grid-column: 2/3;
   grid-row: 1/2;
   background-color: #faf3dd;
+}
+.sidebar {
+  grid-column: 1/2;
+  grid-row: 1/3;
+  background-color: #68b0ab;
+  border-right: 5px solid #4a7c59;
+  border-bottom: 2px;
+}
+.content {
+  grid-column: 2/3;
+  grid-row: 2/3;
+  background-color: #faf3dd;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .print-data {
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    margin: 0.5rem auto;
+    .input-login {
+      padding: 0.5rem 0.5rem;
+      outline: none;
+      border-radius: 0.5rem;
+      border: #08afaf 1.5px solid;
+    }
+    .tag-input-login {
+      font-size: 1.2rem;
+      margin-bottom: 0.2rem;
+    }
+  }
+  .write-data {
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    margin: 0.5rem auto;
+    .input-login {
+      padding: 0.5rem 0.5rem;
+      outline: none;
+      border-radius: 0.5rem;
+      border: #08afaf 1.5px solid;
+      line-height: 70px;
+    }
+    .tag-input-login {
+      font-size: 1.2rem;
+      margin-bottom: 0.2rem;
+    }
+  }
+  .submit-info {
+    border: none;
+    padding: 0.5rem 3rem;
+    border-radius: 0.5rem;
+    background-color: #68b0ab;
+    color: #ffffffff;
+    font-size: 1.2rem;
+    margin-top: 1rem;
+  }
 }
 </style>
